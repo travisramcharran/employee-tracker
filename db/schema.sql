bfdf63a0148f12a7a5e3406fedbd275e28662208
Travis Ramcharran <tc.ramcharran@gmail.com>
2022-09-13 11:44:31 -0500

    made schema eye-friendly

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,48 @@
 DROP DATABASE IF EXISTS company_db;
+
 CREATE DATABASE company_db;
+
 USE company_db;
+
 
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    
     name VARCHAR(30) NOT NULL
 );
 
+
 CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,  
+    
     title VARCHAR(30) NOT NULL,
+    
     salary DECIMAL(10.2) NOT NULL,
+    
     department_id INTEGER,
+    
     INDEX dep_ind (department_id),
+    
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL 
 );
 
+
+
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    
     first_name VARCHAR(30) NOT NULL,
+    
     last_name VARCHAR(30) NOT NULL,
+    
     role_id INTEGER,
+    
     INDEX role_ind (role_id),
+    
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    
     manager_id INTEGER,
+    
     INDEX manager_ind (manager_id),
+    
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
