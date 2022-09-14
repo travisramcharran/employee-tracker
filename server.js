cd01ec48898ea30d76e0117a9ce68e043bf7f310
Travis Ramcharran <tc.ramcharran@gmail.com>
2022-09-13 19:11:06 -0500

    added view employees function

--- server.js
+++ server.js
@@ -112,3 +112,39 @@ viewDepartment = () => {
       }
     );
   };
+
+  viewEmployees = () => {
+    console.log("Displaying employees\n");
+    db.query(
+      `SELECT employee.id,
+
+      employee.first_name,
+      
+      employee.last_name,
+      
+      role.title,
+      
+      department.name,
+      
+      role.salary,
+      
+      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+      
+      FROM employee
+      
+      LEFT JOIN role ON employee.role_id = role.id
+      
+      LEFT JOIN department ON role.department_id = department.id
+      
+      LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+      
+      function (err, results) {
+        
+        if (err) throw err;
+        
+        console.table(results);
+        
+        PromptUser();
+      }
+    );
+  };
